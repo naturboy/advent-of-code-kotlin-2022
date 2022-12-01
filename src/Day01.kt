d4330230f2a1aa4a4ee46b3bdbd9ff59229ca2dc
chris <chris@cbytelabs.de>
2022-12-01 23:42:27 +0100

    add Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,18 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.maxOf { calories -> calories.split("\n").sumOf { it.toInt() } }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map { calories -> calories.split("\n").sumOf { it.toInt() } }.sorted().takeLast(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInputWithDelimiter("Day01_test", "\n\n")
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
-    val input = readInput("Day01")
+    val input = readInputWithDelimiter("Day01", "\n\n")
     println(part1(input))
     println(part2(input))
 }
